02f4ec0134ef72035f42c6a81d874a3b6bb2ed6a
Xianliang Yang <1601210807@pku.edu.cn>
2025-08-22 06:14:42 +0000

    init

--- chat.py
+++
@@ -1,8 +0,0 @@
-import os
-from src.util.llm_client.get_llm_client import get_llm_client
-
-config_file = os.path.join("output", "llm_config", "gpt_4o.json")
-llm_client = get_llm_client(config_file=config_file)
-llm_client.load("Hi. Are you awake?")
-response = llm_client.chat()
-print(response)
