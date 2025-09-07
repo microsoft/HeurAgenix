0982925eb5aff44d2675debb0dfb0c5c6549a402
fung-i <leey34417@gmail.com>
2025-09-07 18:35:22 +0000

    fix tok

--- scripts/weighted_sft.py
+++ scripts/weighted_sft.py
@@ -9,7 +9,7 @@ from alignment.configs import DataConfig, TestConfig
 from alignment.dataset_utils import get_data_collator, load_dataset, load_weight
 from alignment.log import get_log
 from alignment.model_utils import get_model, get_tokenizer
-from scripts.weighted_trainers import WeightedSFTTrainer
+from scripts.weighted_sft_trainers import WeightedSFTTrainer
 from transformers import set_seed
 from trl import ModelConfig, SFTConfig, TrlParser, get_peft_config
 import wandb
